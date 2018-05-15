5ee3c414af7315b35ea4b3a6cf6d1a60f00885f9
Sam Free5ide <snovvcrash@protonmail.com>
2018-05-15 15:21:34 +0300

    Add run time meter utility

---
+++ lib/utils/timing.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+@file timing.py
+@author Sam Freeside <snovvcrash@protonmail.com>
+@date 2018-04
+
+@brief Program run time meter.
+
+@license
+Copyright (C) 2018 Sam Freeside
+
+This file is part of usbrip.
+
+usbrip is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+usbrip is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with usbrip.  If not, see <http://www.gnu.org/licenses/>.
+@endlicense
+"""
+
+import atexit
+import time
+import datetime
+
+
+def tick(msg, fmt='%H:%M:%S', taken=None):
+	#fmt = '%Y-%m-%d %H:%M:%S'
+	now = time.strftime(fmt, time.localtime())
+	print('%s %s' % (msg, now))
+	if taken:
+		taken = datetime.timedelta(seconds=taken)
+		print('[*] Time taken: %s s' % taken)
+
+
+def log():
+	end = time.time()
+	taken = end - start
+	tick('[*] Shutted down at', taken=taken)
+
+
+start = time.time()
+atexit.register(log)
+tick('[*] Started at')
